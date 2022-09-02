16d82b7ab67d9108c81e2c50e901a8e10e4fbab5
chh24k <1095119337@qq.com>
2022-09-02 10:46:57 +0800

    使用Kryo序列化方法

---
+++ rpc-core/src/main/java/code/KryoSerializer.java
@@ -0,0 +1,70 @@
+package code;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import entity.RpcRequest;
+import entity.RpcResponse;
+import enumeration.SerializerCode;
+import exception.SerializeException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+/**
+ * @program: rpc-framework
+ * @description: Use kryo
+ * @author: honghui
+ * @create: 2022/09/02 00:09
+ */
+public class KryoSerializer implements CommonSerializer{
+
+    private static final Logger logger = LoggerFactory.getLogger(KryoSerializer.class);
+
+    private static final ThreadLocal<Kryo> KRYO_THREAD_LOCAL = ThreadLocal.withInitial(() -> {
+        Kryo kryo = new Kryo();
+        kryo.register(RpcResponse.class);
+        kryo.register(RpcRequest.class);
+        kryo.setReferences(true);
+        kryo.setRegistrationRequired(false);
+        return kryo;
+    });
+
+    @Override
+    public int getCode() {
+        return SerializerCode.valueOf("KRYO").getCode();
+    }
+
+    @Override
+    public byte[] serialize(Object o) {
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Output output = new Output(byteArrayOutputStream)) {
+            Kryo kryo = KRYO_THREAD_LOCAL.get();
+            kryo.writeObject(output, o);
+            KRYO_THREAD_LOCAL.remove();
+            return output.toBytes();
+        } catch (Exception e) {
+            logger.error("序列化时有错误发生:", e);
+//            throw new SerializeException("序列化时有错误发生");
+            return null;
+        }
+
+    }
+
+    @Override
+    public Object deserialize(byte[] bytes, Class<?> packageClass) {
+        try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        Input input = new Input(byteArrayInputStream)) {
+            Kryo kryo = KRYO_THREAD_LOCAL.get();
+            Object o = kryo.readObject(input, packageClass);
+            KRYO_THREAD_LOCAL.remove();
+            return o;
+        } catch (Exception e) {
+            logger.error("反序列化时有错误发生:", e);
+//            throw new SerializeException("反序列化时有错误发生");
+            return null;
+        }
+    }
+}
